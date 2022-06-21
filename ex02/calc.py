595aa2a36e2cbd123c3e16016b1e1467998e5ec5
sora <c0a2103473@edu.teu.ac.jp>
2022-06-21 14:47:19 +0900

    3まで #1

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,10 +1,19 @@
 import tkinter as tk
+import tkinter.messagebox as tkm
 
+
+def button_click(event):
+    btn = event.widget
+    num = btn["text"]
+    tkm.showinfo("報告", f"[{num}]が押されました")
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("電卓君.exe")
     root.geometry("300x500")
+
+    #entry = tk.Entry(root, justify="right", width=10, font=(Times New Roman, 40))
+    #entry.grid(row=0, column=0, columnspan=3)
 
     a = 0
     b = 0
@@ -17,5 +26,8 @@ if __name__ == "__main__":
         if b == 3:
             a+=1
             b=0
+        btn.bind("<1>", button_click)
+
+
 
     root.mainloop()
