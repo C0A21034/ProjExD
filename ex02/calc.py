7c60f6d39201f5180eb0a04eceb1a2ecb6df7f2e
sora <c0a2103473@edu.teu.ac.jp>
2022-06-21 15:50:50 +0900

    Cと-等

--- ex02/calc.py
+++ ex02/calc.py
@@ -11,6 +11,12 @@ def button_click(event):
         entry.delete(0, tk.END)
         entry.insert(tk.END, ans)
 
+    elif num == "C":
+        entry.delete(0, tk.END)
+
+    elif num == "破壊":
+        tkm.showwarning("どんまい", "出来ません♨")
+
     else:
         #tkm.showinfo("報告", f"[{num}]が押されました")
         entry.insert(tk.END, num)
@@ -18,18 +24,20 @@ def button_click(event):
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("電卓君.exe")
-    root.geometry("300x600")
+    root.geometry("500x800")
 
     entry = tk.Entry(root,
                     justify="right",
                     width=10,
                     font=("Times New Roman", 40)
                     )
-    entry.grid(row=0, column=0, columnspan=3)
+    entry.grid(row=0, column=0, columnspan=4)
 
     a = 1
     b = 0
-    for i, k in enumerate([9,8,7,6,5,4,3,2,1,0,"+","="]):
+    for i, k in enumerate(["破壊","","","C",9,8,7,
+                          "÷",6,5,4,"×",3,2,1,
+                          "-",0,"","=","+"]):
         btn = tk.Button(root,
                         text=f"{k}",
                         font=("Times New Roman", 30),
@@ -38,7 +46,7 @@ if __name__ == "__main__":
         btn.grid(row=a, column=b)
 
         b+=1
-        if b == 3:
+        if b == 4:
             a+=1
             b=0
         btn.bind("<1>", button_click)
