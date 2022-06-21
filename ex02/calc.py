389ba6df20b4f4223d98d43db62504b5369b6195
sora <c0a2103473@edu.teu.ac.jp>
2022-06-21 14:52:36 +0900

    4まで #2

--- ex02/calc.py
+++ ex02/calc.py
@@ -12,10 +12,14 @@ if __name__ == "__main__":
     root.title("電卓君.exe")
     root.geometry("300x500")
 
-    #entry = tk.Entry(root, justify="right", width=10, font=(Times New Roman, 40))
-    #entry.grid(row=0, column=0, columnspan=3)
+    entry = tk.Entry(root,
+                    justify="right",
+                    width=10,
+                    font=("Times New Roman", 40)
+                    )
+    entry.grid(row=0, column=0, columnspan=3)
 
-    a = 0
+    a = 1
     b = 0
     for i in range(9, -1, -1):
         btn = tk.Button(root, text=f"{i}",font=("Times New Roman", 30),
